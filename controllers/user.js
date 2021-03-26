78460ef6a66e04ce34c8858e70ae2da282077280
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-03-26 13:19:43 -0700

    Added signout for users

--- controllers/user.js
+++ controllers/user.js
@@ -47,7 +47,10 @@ exports.signin = (req, res) => {
   });
 };
 
-
+exports.signout = (req, res) => {
+  res.clearCookie('t');
+  res.json({message: 'Signout success.'});
+}
 
 
 
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 // imports signup controller
-const { signup, signin } = require('../controllers/user');
+const { signup, signin, signout } = require('../controllers/user');
 
 // imports signup validator
 const { userSignupValidator } = require('../validator');
@@ -13,5 +13,6 @@ const router = express.Router();
 
 router.post('/signup', userSignupValidator, signup);
 router.post('/signin', signin);
+router.get('/signout', signout);
 
 module.exports = router;

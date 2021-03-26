3242277d2124c83eaa9461e0fb8cf3d0a3b3a1a5
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-03-26 11:38:40 -0700

    Added user signup validation to API

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
+const expressValidator = require('express-validator');
 require('dotenv').config();
 // import routes
 const userRoutes = require('./routes/user');
@@ -26,6 +27,7 @@ mongoose.connection.on('error', err => {
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
+app.use(expressValidator());
 
 // routes middleware
 app.use('/api', userRoutes);
--- routes/user.js
+++ routes/user.js
@@ -3,11 +3,14 @@ const express = require('express');
 // imports signup controller
 const { signup } = require('../controllers/user');
 
+// imports signup validator
+const { userSignupValidator } = require('../validator');
+
 // router
 const router = express.Router();
 
 
 
-router.post('/signup', signup);
+router.post('/signup', userSignupValidator, signup);
 
 module.exports = router;

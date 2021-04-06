f3f2ad12910b74054e5ddc1442410310a5d5a4e2
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-04-06 12:54:30 -0700

    Added the ability to add a p[roduct photo to the database

--- controllers/product.js
+++ controllers/product.js
@@ -15,13 +15,38 @@ exports.create = (req, res) => {
         error: 'Image could not be uploaded'
       });
     }
+
+    // check for all fields
+    const {
+      name, 
+      description, 
+      price, 
+      category, 
+      quantity, 
+      shipping
+    } = fields;
+    
+    if (!name || !description || !price || !category || !quantity || !shipping) {
+      return res.status(400).json({
+        error: 'All fields are required'
+      });
+    }
+
+    // creates product variable using field info
     let product = new Product(fields);
 
+    // Adds photo data to product if size is under 1mb
     if (files.photo) {
+      if (files.photo.size > 1000000) {
+        return res.status(400).json({
+          error: 'Image should be less than 1mb in size'
+        });
+      }
       product.photo.data = fs.readFileSync(files.photo.path);
       product.photo.contentType = files.photo.type;
     }
 
+    // Saves product to database
     product.save((err, result) => {
       if (err) {
         return res.status(400).json({

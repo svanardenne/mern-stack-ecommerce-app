d34ff34da8f37cf96a0c0c905cbed572cda22a1f
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-04-23 14:28:51 -0700

    Changed route for getting photos from api/products to api/product

--- routes/product.js
+++ routes/product.js
@@ -46,7 +46,7 @@ router.get("/products", list);
 router.get("/products/related/:productId", listRelated);
 router.get("/products/categories", listCategories);
 router.post("/products/by/search", listBySearch);
-router.get("/products/photo/:productId", photo);
+router.get("/product/photo/:productId", photo);
 
 // Params and middleware
 router.param("userId", userById);

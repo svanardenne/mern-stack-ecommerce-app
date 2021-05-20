7a28e19b13890dc499b8f716c73c8bb4516e1052
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-05-20 09:33:45 -0700

    Added method on backend for processing payment

--- routes/braintree.js
+++ routes/braintree.js
@@ -5,12 +5,18 @@ const router = express.Router();
 
 // imports signup controller
 const { userById } = require("../controllers/user");
-const { generateToken } = require("../controllers/braintree");
+const { generateToken, processPayment } = require("../controllers/braintree");
 
 // imports from auth controller
 const { requireSignin, isAuth } = require("../controllers/auth");
 
 router.get("/braintree/getToken/:userId", requireSignin, isAuth, generateToken);
+router.post(
+  "/braintree/payment/:userId",
+  requireSignin,
+  isAuth,
+  processPayment
+);
 
 router.param("userId", userById);
 

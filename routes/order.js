e1e2b2e22ebbab6884109c683bbb5effcf6e266f
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-05-26 11:23:53 -0700

    Added controller method and route for retrieving status values from enum

--- routes/order.js
+++ routes/order.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 // imports signup controller
 const { userById, addOrderToUserHistory } = require("../controllers/user");
-const { create, listOrders } = require("../controllers/order");
+const { create, listOrders, getStatusValues } = require("../controllers/order");
 
 // imports from auth controller
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
@@ -13,6 +13,7 @@ const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 // Imports from product controller
 const { decreaseQuantity } = require("../controllers/product");
 
+// Create order route
 router.post(
   "/order/create/:userId",
   requireSignin,
@@ -22,8 +23,19 @@ router.post(
   create
 );
 
+// Get order list
 router.get("/order/list/:userId", requireSignin, isAuth, isAdmin, listOrders);
 
+// fetches all the status values from enum
+router.get(
+  "/order/status-values/:userId",
+  requireSignin,
+  isAuth,
+  isAdmin,
+  getStatusValues
+);
+
+// Fetches user with id provided from front end in params
 router.param("userId", userById);
 
 module.exports = router;
